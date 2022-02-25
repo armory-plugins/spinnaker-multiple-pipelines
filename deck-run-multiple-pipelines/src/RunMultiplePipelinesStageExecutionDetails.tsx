6e5f6e867adccc042357388f49769a1e2ac1ebbd
Avi1235 <david-prz98@hotmail.com>
2022-02-24 17:16:25 -0700

    deck-module with yaml input

---
+++ deck-run-multiple-pipelines/src/RunMultiplePipelinesStageExecutionDetails.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { ExecutionDetailsSection, IExecutionDetailsSectionProps } from '@spinnaker/core';
+
+/*
+ * You can use this component to provide information to users about
+ * how the stage was configured and the results of its execution.
+ *
+ * In general, you will access two properties of `props.stage`:
+ * - `props.stage.outputs` maps to your SimpleStage's `Output` class.
+ * - `props.stage.context` maps to your SimpleStage's `Context` class.
+ */
+export class RunMultiplePipelinesStageExecutionDetails extends React.Component<IExecutionDetailsSectionProps> {
+  public static title = 'runMultiplePipelines';
+  public render() {
+    return (
+      <ExecutionDetailsSection name={this.props.name} current={this.props.current}>
+        <div>
+          <p>Message: {this.props.stage.outputs.message}</p>
+        </div>
+      </ExecutionDetailsSection>
+    );
+  }
+}
