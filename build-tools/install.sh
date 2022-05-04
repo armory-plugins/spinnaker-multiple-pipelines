72aef26603a10c9e68c5c43d90b0ba421d90e724
Avi1235 <david-prz98@hotmail.com>
2022-05-04 15:57:06 -0600

    test generatePluginsJson

--- build-tools/install.sh
+++ build-tools/install.sh
@@ -9,5 +9,5 @@ VERSION=$(echo "$PLUGIN_ID" | sed 's/smp-//')
 cd /opt/smp
 
 mkdir -p /opt/smp/target/smp/$VERSION
-cp /opt/smp/plugin-info.json /opt/smp/target/smp/plugins.json
+cp /opt/smp/plugins-docker.json /opt/smp/target/smp/plugins.json
 cp /opt/smp/$PLUGIN_FILE /opt/smp/target/smp/$VERSION/$PLUGIN_FILE
