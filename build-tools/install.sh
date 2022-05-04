116bf69677013da81f1aeb7b6ce8d5b0e6f76187
Avi1235 <david-prz98@hotmail.com>
2022-05-04 09:25:18 -0600

    v1.0.2 test

--- build-tools/install.sh
+++ build-tools/install.sh
@@ -9,4 +9,5 @@ VERSION=$(echo "$PLUGIN_ID" | sed 's/smp-//')
 cd /opt/smp
 
 mkdir -p /opt/smp/target/smp/$VERSION
+cp /opt/smp/plugins-info.json /opt/smp/target/smp/plugins.json
 cp /opt/smp/$PLUGIN_FILE /opt/smp/target/smp/$VERSION/$PLUGIN_FILE
