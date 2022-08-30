8455cf7de7938d4e492481fbd49930b82366f4e7
David Perez <43676929+Avi1235@users.noreply.github.com>
2022-08-30 15:36:36 -0600

    changed tests to java (#15)

---
+++ orca-run-multiple-pipelines/src/test/java/io/armory/plugin/smp/RunMultiplePipelinesIntegrationTest.java
@@ -0,0 +1,18 @@
+package io.armory.plugin.smp;
+
+import com.netflix.spinnaker.orca.api.pipeline.graph.StageDefinitionBuilder;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RunMultiplePipelinesIntegrationTest extends RunMultiplePipelinesFixture {
+
+    @Test
+    public void resolveToCorrectTypeTest() {
+        System.out.println(this.stageResolver);
+        StageDefinitionBuilder stageDefinitionBuilder = this.stageResolver.getStageDefinitionBuilder(
+                RunMultiplePipelinesStage.class.getTypeName(), "runMultiplePipelines");
+
+        assertEquals(stageDefinitionBuilder.getType(), "runMultiplePipelines" , "Expected stageDefinitionBuilder to be of type runMultiplePipelines");
+    }
+}
